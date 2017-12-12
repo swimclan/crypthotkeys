3db89a447a492b5d50b904fc0dd77a20634d4efc
Matthew Herron <matthew.herron77@gmail.com>
2017-12-11 22:31:33 -0600

    Added orders fetching endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -19,3 +19,21 @@ module.exports.getAccount = (req, res, next) => {
     return res.status(200).json(data);
   });
 }
+
+module.exports.getOrders = (req, res, next) => {
+  exchange.authedClient.getOrders((err, resp, data) => {
+    if (err) {
+      return res.status(500).json({error: err});
+    }
+    return res.status(200).json(data);
+  });
+}
+
+module.exports.getOrder = (req, res, next) => {
+  exchange.authedClient.getOrder(req.params.id, (err, resp, data) => {
+    if (err) {
+      return res.status(500).json({error: err});
+    }
+    return res.status(200).json(data);
+  });
+}
