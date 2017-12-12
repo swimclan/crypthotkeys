727e20d5a3c9ea5846a07dee758455a8a5107a43
Matthew Herron <matthew.herron77@gmail.com>
2017-12-11 22:14:32 -0600

    Added getAccount endpoint

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,16 @@ var gdax = require('../lib/gdax');
 const exchange = gdax.getInstance();
 
 module.exports.getAccounts = (req, res, next) => {
-  console.log(exchange);
   exchange.authedClient.getAccounts((err, resp, data) => {
+    if (err) {
+      return res.status(500).json({error: err});
+    }
+    return res.status(200).json(data);
+  });
+}
+
+module.exports.getAccount = (req, res, next) => {
+  exchange.authedClient.getAccount(req.params.id, (err, resp, data) => {
     if (err) {
       return res.status(500).json({error: err});
     }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var controllers = require('../controllers');
 
-/* GET Account Profile. */
+/* GET Accounts */
 router.get('/accounts', controllers.getAccounts);
+
+/* GET Account */
+router.get('/account/:id', controllers.getAccount);
 
 module.exports = router;
