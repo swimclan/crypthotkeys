b4ed050381395016b89a80c837b7206f6a7015bd
Matthew Herron <matthew.herron77@gmail.com>
2017-12-11 20:37:51 -0600

    Initial changelog and version patch

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/v1', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
